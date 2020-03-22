3cc2b2d9b275be1986deff407761d0e591ae9c56
Udal Pal <paludal786@gmail.com>
2020-03-22 19:41:35 +0530

    install socket pkg

---
+++ index.js
@@ -0,0 +1,19 @@
+let app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function() {
+    console.log('Connection Done On 3000 Port !.....');
+});
+
+io.on('connection', (socket) => {
+    console.log('Socket Connected !.....');
+
+    socket.on('disconnect', () => {
+        console.log('Socket Disconnected !...');
+    })
+
+});
